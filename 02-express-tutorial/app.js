3820969be5fca4717c99ea4ade9416471bdb9da1
Alena Dudko <e.v.dudko@gmail.com>
2024-03-13 21:17:24 -0500

    Work in progress. Set up basic Express application.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,15 @@
 console.log('Express Tutorial')
+const express = require('express');
+const app = express();
+
+//setup static and middleware
+app.use(express.static('./public'));
+
+app.all('*', (req, res) => {
+    res.status(404).send('Resource not found')
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+})

c950d7b790716296d9fe66a566fdf2ea9cdca69e
Alena Dudko <e.v.dudko@gmail.com>
2024-04-18 23:55:02 -0500

    week8

---
+++ 05-JWT-Basics/starter/errors/index.js
@@ -0,0 +1,10 @@
+const CustomAPIError = require('./custom-error')
+const BadRequestError = require('./bad-request')
+const UnauthenticatedError = require('./unauthenticated.js')
+
+module.exports = {
+  CustomAPIError,
+  BadRequestError,
+  UnauthenticatedError,
+}
+
